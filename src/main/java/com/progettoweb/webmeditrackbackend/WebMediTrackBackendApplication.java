3b7327e637756ea1651bb12c2152ebd06b698f78
Fabio Ardis <fabioardis@gmail.com>
2024-01-04 11:31:39 +0100

    Initial commit

---
+++ src/main/java/com/progettoweb/webmeditrackbackend/WebMediTrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.progettoweb.webmeditrackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMediTrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMediTrackBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/progettoweb/webmeditrackbackend/WebMediTrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progettoweb.webmeditrackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMediTrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
